65154090c0c64b808261a130bc1aa4659d9892b2
Adel Kazem <adelkazem.1991@gmail.com>
2025-03-28 13:45:09 +0200

    whats

--- products.js
+++ products.js
@@ -6,5 +6,5 @@ const PRODUCTS = [
         price: 29.99,
         rating: 4.9,
         images: ['https://images.unsplash.com/photo-1588168333986-5078d3ae3976?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'],
-    },
+    }
 ];
