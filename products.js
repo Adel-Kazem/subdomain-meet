0b33148bf15c464b1a8221f6839c2e0e9918d04f
Adel Kazem <adelkazem.1991@gmail.com>
2025-03-28 13:44:49 +0200

    whats

--- products.js
+++ products.js
@@ -5,7 +5,6 @@ const PRODUCTS = [
         description: 'Our signature ribeye steak, known for its rich marbling and exceptional flavor. Perfect for grilling or pan-searing to your desired doneness.',
         price: 29.99,
         rating: 4.9,
-        stock: 25,
         images: ['https://images.unsplash.com/photo-1588168333986-5078d3ae3976?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80'],
     },
 ];
